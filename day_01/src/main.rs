fc465452062401d88a87d4f3549bd29f35091983
korbinian <korbinianfl98@googlemail.com>
2024-12-01 12:36:07 +0100

    finish day_01

---
+++ day_01/src/main.rs
@@ -0,0 +1,40 @@
+// const INPUT: &'static str = include_str!("../test.txt");
+const INPUT: &'static str = include_str!("../input");
+
+fn main() {
+    let lines = INPUT.lines();
+    let mut left = Vec::new();
+    let mut right = Vec::new();
+
+    for line in lines {
+        let mut values = line.split_ascii_whitespace();
+        left.push(values.next().unwrap().parse::<i32>().unwrap());
+        right.push(values.next().unwrap().parse::<i32>().unwrap());
+    }
+    left.sort();
+    right.sort();
+    let mut curr_idx = 0;
+    let mut diff = 0;
+    let mut res = 0;
+    let mut occurance = 0;
+    let mut last_left = 0;
+    for (a, b) in left.iter().zip(right.iter()) {
+        diff += (a - b).abs();
+        if *a != last_left {
+            occurance = 0;
+        }
+        for el in right[curr_idx..].iter() {
+            if el > a {
+                break;
+            }
+            if el == a {
+                occurance += 1;
+            }
+            curr_idx += 1;
+        }
+        last_left = *a;
+        res += a * occurance;
+    }
+    println!("Part 1: {diff}");
+    println!("Part 2: {res}")
+}
