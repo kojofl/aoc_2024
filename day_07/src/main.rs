2a5e8c756c0fb4e6b43bf33706c6d877ee766201
korbinian <korbinianfl98@googlemail.com>
2024-12-07 12:42:33 +0100

    big performance

--- day_07/src/main.rs
+++ day_07/src/main.rs
@@ -1,6 +1,6 @@
 use std::{collections::VecDeque, usize};
 
-//  static INPUT: &'static str = include_str!("../test");
+// static INPUT: &'static str = include_str!("../test");
 static INPUT: &'static str = include_str!("../input");
 
 fn main() {
@@ -36,11 +36,13 @@ impl OperationFacility {
         state_queue.push_back(OperationState::new(1, self.operands[0]));
 
         while let Some(state) = state_queue.pop_front() {
-            if state.val_idx < self.operands.len() {
-                let val = self.operands[state.val_idx];
-                if state.advance_state(&mut state_queue, val, self.target) {
+            if state.val_idx >= self.operands.len() {
+                if self.target == state.state {
                     return true;
                 }
+            } else {
+                let val = self.operands[state.val_idx];
+                state.advance_state(&mut state_queue, val, self.target);
             }
         }
         false
@@ -61,36 +63,32 @@ impl OperationState {
         }
     }
 
-    fn advance_state(self, queue: &mut VecDeque<OperationState>, val: u64, target: u64) -> bool {
+    fn advance_state(self, queue: &mut VecDeque<OperationState>, val: u64, target: u64) {
         // Add
         let add = self.state + val;
-        if add == target {
-            return true;
-        }
-        if add < target {
+        if add <= target {
             queue.push_back(OperationState::new(self.val_idx + 1, add));
         }
         // Mul
         let mul = self.state * val;
-        if mul == target {
-            return true;
-        }
-        if mul < target {
+        if mul <= target {
             queue.push_back(OperationState::new(self.val_idx + 1, mul));
         }
         // Concat
         let con = concat_nums(self.state, val);
-        if con == target {
-            return true;
-        }
-        if con < target {
+        if con <= target {
             queue.push_back(OperationState::new(self.val_idx + 1, con));
         }
-        false
     }
 }
 
+#[test]
+fn concat_test() {
+    let c = concat_nums(16, 10);
+    assert_eq!(c, 1610)
+}
+
 fn concat_nums(l: u64, r: u64) -> u64 {
-    let l = l << (r as f64).log10().floor() as u32;
+    let l = l * 10_u64.pow(((r as f64).log10() + 1.0).floor() as u32);
     l + r
 }
